d36b4ca4ad49bdac9f351b3e5817eddd111d9e0f
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-04-26 20:20:01 -0700

    added questions/partial js functiionality

---
+++ assets/script.js
@@ -0,0 +1,77 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "Who was the first team to win the CFP?",
+        choice1: "Alabama",
+        choice2: "Clemson",
+        choice3: "Ohio State",
+        choice4: "Georgia",
+        answer: 3
+    },
+    {
+        question: "Which General led the Continental Army in the Revolutionary War?",
+        choice1: "George Washington",
+        choice2: "Francisco Franco",
+        choice3: "Napoleon Bonaparte",
+        choice4: "Julius Cesar",
+        answer: 1
+    },
+    {
+        question: "Which of these beers are made in Pennsylvania?",
+        choice1: "Budweiser",
+        choice2: "Yuengling",
+        choice3: "Heineken",
+        choice4: "Shiner Bock",
+        answer: 2
+    }
+]
+
+
+const Max_Questions = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+}
+
+getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        getNewQuestion();
+    });
+});
+
+
+
+startGame();
